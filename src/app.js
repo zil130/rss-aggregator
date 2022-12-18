37e08811c261e25e73c285a6cc51bc012d9917fd
zil130 <55101149+zil130@users.noreply.github.com>
2022-12-18 01:31:21 -0300

    feat: validation in the form of adding an RSS feed

---
+++ src/app.js
@@ -0,0 +1,45 @@
+import { string } from 'yup';
+import onChange from 'on-change';
+
+export default () => {
+  const state = {
+    feeds: [],
+    error: [],
+  };
+
+  const form = document.querySelector('.rss-form');
+  const field = document.querySelector('[aria-label="url"]');
+  field.focus();
+  const feedback = document.querySelector('.feedback');
+
+  const schema = string().url('Ссылка должна быть валидным URL').notOneOf([state.feeds], 'RSS уже существует');
+
+  const validate = (input) => {
+    try {
+      schema.validateSync(input, { abortEarly: false });
+      state.feeds.push(input);
+      return [];
+    } catch (e) {
+      return e.inner;
+    }
+  };
+
+  const watchedState = onChange(state, (path, value) => {
+    if (path === 'error') {
+      if (value.length) {
+        field.classList.add('is-invalid');
+        feedback.textContent = value[0].message;
+      } else {
+        field.classList.remove('is-invalid');
+        feedback.textContent = '';
+        form.reset();
+        field.focus();
+      }
+    }
+  });
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    watchedState.error = validate(field.value);
+  });
+};
