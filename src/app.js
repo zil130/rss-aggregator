eb07c8b1e9e22b1bbc56fdbb6258280d884873e1
zil130 <index.che@gmail.com>
2022-12-20 02:15:10 -0300

    feat: add view

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import { string } from 'yup';
-import onChange from 'on-change';
+import watcher, { form, field } from './view.js';
 
 export default () => {
   const state = {
@@ -7,33 +7,16 @@ export default () => {
     error: [],
   };
 
-  const form = document.querySelector('.rss-form');
-  const field = document.querySelector('[aria-label="url"]');
-  field.focus();
-  const feedback = document.querySelector('.feedback');
-
   const schema = string().url('Ссылка должна быть валидным URL').notOneOf([state.feeds], 'RSS уже существует');
   const validate = (input) => schema.validate(input, { abortEarly: false });
 
-  const watchedState = onChange(state, (path, value) => {
-    if (path === 'error') {
-      if (value.length) {
-        field.classList.add('is-invalid');
-        feedback.textContent = value[0].message;
-      } else {
-        field.classList.remove('is-invalid');
-        feedback.textContent = '';
-        form.reset();
-        field.focus();
-      }
-    }
-  });
+  const watchedState = watcher(state);
 
   form.addEventListener('submit', (event) => {
     event.preventDefault();
     validate(field.value)
       .then((link) => {
-        state.feeds.push(link);
+        watchedState.feeds.push(link);
         watchedState.error = [];
       })
       .catch((error) => {
