ef29011da6783ed37097ead8bebc1538f328af10
zil130 <index.che@gmail.com>
2022-12-20 12:43:43 -0300

    refactor: naming

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import { string } from 'yup';
-import watcher, { form, field } from './view.js';
+import watcher, { form, inputField } from './view.js';
 
 export default () => {
   const state = {
@@ -8,19 +8,18 @@ export default () => {
   };
 
   const schema = string().url('Ссылка должна быть валидным URL').notOneOf([state.feeds], 'RSS уже существует');
-  const validate = (input) => schema.validate(input, { abortEarly: false });
-
+  const validate = (url) => schema.validate(url, { abortEarly: false });
   const watchedState = watcher(state);
 
   form.addEventListener('submit', (event) => {
     event.preventDefault();
-    validate(field.value)
-      .then((link) => {
-        watchedState.feeds.push(link);
+    validate(inputField.value)
+      .then((url) => {
+        watchedState.feeds.push(url);
         watchedState.error = [];
       })
-      .catch((error) => {
-        watchedState.error = error.inner;
+      .catch((e) => {
+        watchedState.error = e.inner;
       });
   });
 };
--- src/view.js
+++ src/view.js
@@ -1,29 +1,29 @@
 import onChange from 'on-change';
 
 export const form = document.querySelector('.rss-form');
-export const field = document.querySelector('[aria-label="url"]');
-field.focus();
+export const inputField = document.querySelector('[aria-label="url"]');
+inputField.focus();
 const feedback = document.querySelector('.feedback');
 
-const renderValid = (value) => {
-  field.classList.add('is-invalid');
+const renderInvalid = (value) => {
+  inputField.classList.add('is-invalid');
   feedback.textContent = value[0].message;
 };
 
-const renderInvalid = () => {
-  field.classList.remove('is-invalid');
+const renderValid = () => {
+  inputField.classList.remove('is-invalid');
   feedback.textContent = '';
   form.reset();
-  field.focus();
+  inputField.focus();
 };
 
 export default (state) => {
   const watchedState = onChange(state, (path, value) => {
     if (path === 'error') {
       if (value.length) {
-        renderValid(value);
+        renderInvalid(value);
       } else {
-        renderInvalid();
+        renderValid();
       }
     }
   });
