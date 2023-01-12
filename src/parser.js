d855647404b486c6ade0dad677427f9b1271646c
zil130 <index.che@gmail.com>
2023-01-12 03:33:29 -0300

    refactor: parser xml

--- src/parser.js
+++ src/parser.js
@@ -1,4 +1,25 @@
 export default (xmlString) => {
   const parser = new DOMParser();
-  return parser.parseFromString(xmlString, 'application/xml');
+  const xmlDocument = parser.parseFromString(xmlString, 'application/xml');
+
+  try {
+    const feed = {
+      title: xmlDocument.querySelector('title').textContent.trim(),
+      description: xmlDocument.querySelector('description').textContent.trim(),
+    };
+    const xmlDocumentItems = xmlDocument.querySelectorAll('item');
+    const posts = Array.from(xmlDocumentItems).map((xmlDocumentItem) => {
+      const link = xmlDocumentItem.querySelector('link').textContent.trim();
+      const title = xmlDocumentItem.querySelector('title').textContent.trim();
+      const description = xmlDocumentItem.querySelector('description').textContent.trim();
+
+      return {
+        link, title, description,
+      };
+    });
+
+    return { feed, posts };
+  } catch {
+    throw new Error('feedback.failure.notContainValidRSS');
+  }
 };
